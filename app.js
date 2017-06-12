beb119ba1fc52cfd2ff88221b0e8e8a91a59a641
Amanda Zheng <az0910@icloud.com>
2017-06-12 13:50:48 -0700

    fish pic and wired js/css/html

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+var numbers = [3,4,5];
+var multipliedNumbers = ['potato'];
+
+function timesNine() {
+  for (var i = 0; i < numbers.length; i++){
+    multipliedNumbers.push(numbers[i] * 9);
+  }
+}
+
+timesNine();
+console.log(multipliedNumbers);
